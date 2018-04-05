37ebd3463d1cb119ef8ccc2a459133694809b511
Stasik <scherkasov8@gmail.com>
2018-04-05 23:45:00 +0300

    added asserts but not to Circle().perimeter

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -74,12 +74,28 @@ circleArea.area
 var circlePerimeter = Circle(radius: 5)
 circlePerimeter.perimeter
 
+//assert to Circle
+let delta: Float = pow(10, -9)
+let radius: Float = 12
+assert(abs(Circle(radius: radius).area - pow(radius, 2) * Float.pi) < delta)
+// I don't know how to check perimrter(
+
 var squereArea = Squere(side: 10)
 squereArea.area
 var squerePerimeter = Squere(side: 11)
 squerePerimeter.perimeter
 
+//assert to Squere
+let height: Float = 34
+assert(Squere(side: height).area == pow(height,  2))
+assert(Squere(side: height).perimeter == height * 4)
+
 var rectangleArea = Rectangle(height: 12, width: 5)
 rectangleArea.area
 var rectanglePerimeter = Rectangle(height: 10, width: 3)
 rectanglePerimeter.perimeter
+
+//assert to Rectangle
+let width: Float = 5
+assert(Rectangle(height: height, width: width).area == height * width)
+assert(Rectangle(height: height, width: width).perimeter == 2 * (height + width))
