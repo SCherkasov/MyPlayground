13641a4eb198c9e73c0e78820ed101904651425e
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2018-04-13 12:44:20 -0700

    refactoring

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,81 +2,65 @@
 
 import UIKit
 
-class Shape: NSObject, NSCopying {
-   
-    var area: Float
-    var perimeter: Float
-    
-    init(area: Float, perimeter: Float) {
-        self.area = area
-        self.perimeter = perimeter
-    }
-    
-    func copy(with zone: NSZone? = nil) -> Any {
-        let copy = Shape(area: area, perimeter: perimeter)
-        return copy
-    }
+protocol Shape: NSCopying {
+    var area: Float { get }
+    var perimeter: Float { get }
 }
 
-class Circle: Shape {
+class Circle: NSObject, Shape {
 
-    override var area: Float {
+    var radius: Float
+    
+    var area: Float {
         get {
             return Float.pi * pow(self.radius, 2)
         }
-        set {
-            
-        }
     }
     
-   override  var perimeter: Float {
+   var perimeter: Float {
         get {
             return Float.pi * (2 * self.radius)
         }
-        set {
-            
-        }
     }
-    
-    private var radius: Float
     
     init(radius: Float) {
         self.radius = radius
-        super.init(area: area, perimeter: perimeter)
+    }
+    
+    func copy(with zone: NSZone? = nil) -> Any {
+        return Circle(radius: self.radius)
     }
 }
 
 class Rectangle: Shape {
     
-    override var area: Float {
+    var width: Float
+    var height: Float
+    
+    var area: Float {
         get {
             return self.height * self.width
         }
-        set {
-            
-        }
     }
     
-    override var perimeter: Float {
+    var perimeter: Float {
         get {
             return 2 * (self.height + self.width)
         }
-        set {
-            
-        }
     }
     
-    internal var height: Float
-    internal var width: Float
-    
-    init(height: Float, width: Float) {
-        self.height = height
+    init(width: Float, height: Float) {
         self.width = width
-        super.init(area: area, perimeter: perimeter)
+        self.height = height
+    }
+    
+    func copy(with zone: NSZone? = nil) -> Any {
+        return Rectangle(width: self.width, height: self.height)
     }
 }
 
 class Squere: Rectangle {
+    var color: UIColor
     
     private var side: Float {
         get {
@@ -88,39 +72,42 @@ class Squere: Rectangle {
         }
     }
     
-    init(side: Float) {
-        super.init(height: side, width: side)
+    init(side: Float, color: UIColor) {
+        self.color = color
+        super.init(width: side, height: side)
+    }
+    
+    override func copy(with zone: NSZone?) -> Any {
+        let square  = super.copy(with: zone) as! Squere
+        square.color = self.color
+        
+        return square
     }
 }
 
-var circleArea = Circle(radius: 12)
-circleArea.area
-var circlePerimeter = Circle(radius: 5)
-circlePerimeter.perimeter
+let delta = Float(pow(10, -9))
 
-//assert to Circle
-let delta: Float = pow(10, -9)
-let radius: Float = 12
-assert(abs(Circle(radius: radius).area - pow(radius, 2) * Float.pi) < delta)
-// I don't know how to check perimrter(
+// Test for circle area
+assert(abs(Circle(radius: 12).area - (pow(12, 2) * Float.pi)) < delta)
 
-var squereArea = Squere(side: 10)
-squereArea.area
-var squerePerimeter = Squere(side: 11)
-squerePerimeter.perimeter
+// Test for circle perimeter
+assert(abs(Circle(radius: 5).perimeter - 2 * 5 * Float.pi) < delta)
 
-//assert to Squere
-let height: Float = 34
-assert(Squere(side: height).area == pow(height,  2))
-assert(Squere(side: height).perimeter == height * 4)
+// Test for square area
+assert(Squere(side: 10, color: .green).area == pow(10, 2))
 
-var rectangleArea = Rectangle(height: 12, width: 12)
-rectangleArea.area
-var squere = rectangleArea.copy() as! Rectangle
-var rectanglePerimeter = Rectangle(height: 10, width: 3)
-rectanglePerimeter.perimeter
+// Test for square perimeter
+assert(Squere(side: 11, color: .blue).perimeter == 11 * 4)
 
-//assert to Rectangle
-let width: Float = 5
-assert(Rectangle(height: height, width: width).area == height * width)
-assert(Rectangle(height: height, width: width).perimeter == 2 * (height + width))
+// Test for rectangle area
+assert(Rectangle(width: 12, height: 10).area == 12 * 10)
+
+// Test for rectangle perimeter
+assert(Rectangle(width: 3, height: 10).perimeter == 2 * (3 + 10))
+
+// Test for copy
+let rect = Rectangle(width: 3, height: 10)
+assert(rect != (rect.copy() as! Rectangle))
+assert(rect.width == (rect.copy() as! Rectangle).width)
+assert(rect.height == (rect.copy() as! Rectangle).height)
+
